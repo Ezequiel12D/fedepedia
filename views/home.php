1d33ed7bed64ac7594babf9f3ee7d19eff18b8f5
Tsifok <hermanmatias03@gmail.com>
2023-05-04 12:42:09 -0300

    cincuenta porciento del modelado del home

--- views/home.php
+++ views/home.php
@@ -1,39 +1,59 @@
 <?php include_once ("layout.php"); ?>
 
 
-<div id="sidebar">
+<div id="left-bar">
     
     <div class="container-logo">
         <img src="../images/logo.png" class="rounded float-left img-logo" alt="img_logo">
     </div>
     
-    <div id="lh-1"></div>
+    <div class="lh"></div>
     
     <div id="search-bar">        
         <nav class="navbar navbar-light">
             <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control mr-sm-2" style="width: 70%;" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </nav>
     </div>
-
-    <div>
+    <div class="lh" style="top:35%"></div>
+    <div id="link-bar">
         <ul>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
+            <li><a href="">Portada</a></li>
+            <li><a href="">Articulos destacados</a></li>
+            <li><a href="">Articulos nuevos</a></li>
+            <li><a href="">Articulo aleatorio/a></li>
+            <li><a href="">Configuracion</a></li>
+            <li><a href="">Crear articulo</a></li>
+            <li><a href="">Mis articulos</a></li>
+            <li><a href="">Articulos guardados</a></li>
+            <li><a href="">Filtrar</a></li>
+            <li><a href="">Ayuda</a></li>
+            <li><a href="">DENUNCIAS</a></li>
         </ul>
     </div>
 
 </div>
 
-<div>
-    
+<div id="center-bar1">
+    <div id="bienvenida">
+        <div class="float-right lol">Hay xxxxxx articulos</div>
+
+        <h2>Bienvenido a Fedepedia</h2>
+        <p>
+            Fedepedia es una pagina donde nos preocupamos por darle la mejor experiencia en la consulta de articulos online.<br>
+            Trataremos de mostrarle un nuevo mundo de posivilidades, leera los articulos creados directamente por los usuarios.
+        </p>        
+    </div>
+    <div>articulo</div>
 </div>
 
-<div>
 
+<div id="right-bar">
+    <div>
+        <div id="user_profile" class="float-left">asdf</div>
+    </div>
 </div>
 
 
