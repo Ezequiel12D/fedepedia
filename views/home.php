b619abc3d6957b2a0b7ba8c266edfbb5a5af0546
Tsifok <hermanmatias03@gmail.com>
2023-05-04 11:18:26 -0300

    creo lo anterior

---
+++ views/home.php
@@ -0,0 +1,69 @@
+<?php include_once ("layout.php"); ?>
+
+
+<div id="sidebar">
+    
+    <div class="container-logo">
+        <img src="../images/logo.png" class="rounded float-left img-logo" alt="img_logo">
+    </div>
+    
+    <div id="lh-1"></div>
+    
+    <div id="search-bar">        
+        <nav class="navbar navbar-light">
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+    </div>
+
+    <div>
+        <ul>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+        </ul>
+    </div>
+
+</div>
+
+<div>
+    
+</div>
+
+<div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/jquery/bootstrap.min.js"></script>
+<script src="../js/jquery/jquery-3.4.1.slim.min.js"></script>
+<script src="../js/jquery/popper.min.js"></script>
