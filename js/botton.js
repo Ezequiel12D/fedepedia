342967cd24a07f0b710a7b1d63d8c2491e5ac15e
Ezequiel12D <ezequeil.sosa.et26@gmail.com>
2023-05-19 15:14:09 -0300

    al pasar el click por los botones se alternan los colores

--- js/botton.js
+++ js/botton.js
@@ -1,15 +1,9 @@
-// Código JavaScript para mostrar una alerta después de enviar el formulario
 document.getElementById("upload-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
+    event.preventDefault();
 
-    // Aquí puedes agregar la lógica para procesar la subida de la publicación
-
-    // Obtener solo el nombre del dominio
     var domain = window.location.hostname;
 
-    // Mostrar una alerta de publicación subida sin "localhost"
     alert("¡Publicación subida con éxito en " + domain + "!");
 
-    // Redireccionar al home después de procesar la subida
-    window.location.href = "../views/"; // Reemplaza "home.html" con la URL de tu página home
+    window.location.href = "../views/";
 });
