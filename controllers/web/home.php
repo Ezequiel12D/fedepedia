84c902bc5b0fe4b7a5fa7f41a8b3980221b0ce53
Tsifok <hermanmatias03@gmail.com>
2023-06-28 21:47:10 -0300

    implementando MVC
    solo el inicio

---
+++ controllers/web/home.php
@@ -0,0 +1,7 @@
+<?php 
+
+/*ACA DESPUES VAN LOS ARTICULOS QUE APARECENT EN HOME*/
+
+$page = "Inicio";
+$section = "home";
+require_once "../../views/layout.php";
--- views/layout.php
+++ views/layout.php
@@ -8,17 +8,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../images/logo.png">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/style_layout.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/style_layout.css">
+    
     <title>Home</title>
 </head>
 
 <body>
     <div id="container">
-        <?php require_once "home.php"; ?>
+        <?php require_once($section . ".php") ?>
     </div>
-    <script src="../js/jquery/jquery-3.4.1.slim.min.js"></script>
-    <script src="../js/jquery/popper.min.js"></script>
+    <script src="../../js/jquery/jquery-3.4.1.slim.min.js"></script>
+    <script src="../../js/jquery/popper.min.js"></script>
 </body>
 
 </html>
